27fb9d2f2478d9319f8ad3847e040b290837cc4f
Furkan Gozubuyuk <furkan.gozubuyukk@gmail.com>
2025-09-30 06:18:31 +0300

    BlogSite

---
+++ myBlog/public/js/scroll.js
@@ -0,0 +1,25 @@
+ const targets = document.querySelectorAll('.target')
+
+const options = {
+    threshold: 0.3
+}
+
+const callBack = (entries, observer) => {
+    entries.forEach(entry => {
+     if(entry.isIntersecting){
+         entry.target.classList.add('active')
+         
+     }
+     else{
+            entry.target.classList.remove('active')
+     }
+    })
+}
+
+
+
+const observer = new IntersectionObserver(callBack, options);
+
+targets.forEach(target => {
+    observer.observe(target)
+})
